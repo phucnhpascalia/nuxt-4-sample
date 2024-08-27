6b5d8016925d368435a936bc3bf8e9fa0c5096fc
PhucNH <phucnhpascalia>
2024-08-27 23:16:39 +0700

    fix: update package

--- playwright.config.ts
+++ playwright.config.ts
@@ -57,7 +57,7 @@ export default defineConfig<ConfigOptions>({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "yarn run dev",
+    command: "yarn run start",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
